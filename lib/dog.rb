82c68433b8a1706966f73dccd0615fba98b40f0b
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-07 18:00:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,11 @@
 class Dog
+  attr_accessor :id, :name, :breed
+  def initialize(attributes)
+    attributes.each {|key, var| self.send("#{key}=", value)}
+  end
+
+  def self.create(attributes)
+    Dog.new(attributes)
+  end
+
 end
