e8a5a85fa35b3e25ab27313742b5baebe8a00648
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-07 18:09:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,6 @@
 require_relative '../bin/environment.rb'
 
 class Dog < ActiveRecord::Base
-  # attr_accessor :id, :name, :breed
-  # # def initialize(attributes)
-  # #   attributes.each {|key, value| self.send("#{key}=", value)}
-  # # end
-
 
 
 end
