736007f4013588e4290e313c495fa67d0c5b5489
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-07 18:01:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,8 @@
-class Dog
+class Dog < ActiveRecord::Base
   attr_accessor :id, :name, :breed
   def initialize(attributes)
     attributes.each {|key, var| self.send("#{key}=", value)}
   end
 
-  def self.create(attributes)
-    Dog.new(attributes)
-  end
 
 end
